bdcd76c90320302fe0444b1dd5c909aa168ab785
kaiquye <kaiquemendesilva82@gmail.com>
2022-05-25 07:59:16 -0300

    migrate : configurando migrate, nova tabela : endereco

---
+++ project/migrations/20220525105718_create_table_endereco.js
@@ -0,0 +1,19 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('ENDERECO', function (table) {
+        table.increments('id').unsigned().primary();
+        table.string('cep').notNullable();
+        table.string('logradouro');
+        table.string('complemento');
+        table.string('bairro');
+        table.string('uf');
+        table.bigInteger('proprietario_id').unsigned()
+            .index()
+            .references('id')
+            .inTable('PROPRIETARIO');
+    });
+};
+
+exports.down = function (knex) {
+
+};
